2d18613ced4b03d3f923254aab52d0b373d515aa
muhammad-sharopov <82517976+muhammad-sharopov@users.noreply.github.com>
2025-03-06 22:14:10 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,144 +18,133 @@ def load_data():
 
 data = load_data()
 
-# Создаем сайдбар с кнопками
-option = st.sidebar.selectbox('Выберите раздел:', ['Препроцессинг данных', 'Модели и результаты'])
+st.title('Анализ данных о депрессии студентов')
+st.write("Информация о данных:")
+st.write(data.info())
+st.write(data.describe())
 
-# Раздел 1: Препроцессинг данных
-if option == 'Препроцессинг данных':
-    st.title('Анализ данных о депрессии студентов')
-    st.write("Информация о данных:")
-    st.write(data.info())
-    st.write(data.describe())
+data['Financial Stress'].fillna(data['Financial Stress'].median(), inplace=True)
+st.write("Пропущенные значения после иммутатции:")
+st.write(data.isnull().sum())
 
-    # Заполнение пропусков
-    data['Financial Stress'].fillna(data['Financial Stress'].median(), inplace=True)
-    st.write("Пропущенные значения после иммутатции:")
-    st.write(data.isnull().sum())
+st.subheader('Распределение по полу')
+gender_counts = data['Gender'].value_counts()
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.pie(gender_counts, labels=gender_counts.index, autopct='%1.1f%%', startangle=90,
+       colors=['#66b3ff', '#ff9999'], shadow=True, explode=(0.05, 0), textprops={'fontsize': 14})
+ax.set_title('Распределение по полу', fontsize=16, fontweight='bold', pad=20)
+st.pyplot(fig)
 
-    st.subheader('Распределение по полу')
-    gender_counts = data['Gender'].value_counts()
-    fig, ax = plt.subplots(figsize=(8, 8))
-    ax.pie(gender_counts, labels=gender_counts.index, autopct='%1.1f%%', startangle=90,
-           colors=['#66b3ff', '#ff9999'], shadow=True, explode=(0.05, 0), textprops={'fontsize': 14})
-    ax.set_title('Распределение по полу', fontsize=16, fontweight='bold', pad=20)
-    st.pyplot(fig)
+st.subheader('Распределение возрастов')
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.hist(data['Age'], bins=15, color='skyblue', edgecolor='black')
+ax.set_title('Распределение возрастов')
+ax.set_xlabel('Возраст')
+ax.set_ylabel('Частота')
+ax.grid(True)
+st.pyplot(fig)
 
-    st.subheader('Распределение возрастов')
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.hist(data['Age'], bins=15, color='skyblue', edgecolor='black')
-    ax.set_title('Распределение возрастов')
-    ax.set_xlabel('Возраст')
-    ax.set_ylabel('Частота')
-    ax.grid(True)
-    st.pyplot(fig)
+st.subheader('Корреляционная матрица')
+numerical_data = data.select_dtypes(include=['float64', 'int64'])
+correlation_matrix = numerical_data.corr()
 
-    st.subheader('Корреляционная матрица')
-    numerical_data = data.select_dtypes(include=['float64', 'int64'])
-    correlation_matrix = numerical_data.corr()
+fig, ax = plt.subplots(figsize=(12, 10))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=0.5, linecolor='black',
+            cbar_kws={'shrink': 0.8}, annot_kws={'size': 10, 'weight': 'bold'})
+ax.set_title('Корреляционная матрица числовых признаков', fontsize=18, fontweight='bold', pad=20)
+st.pyplot(fig)
 
-    fig, ax = plt.subplots(figsize=(12, 10))
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=0.5, linecolor='black',
-                cbar_kws={'shrink': 0.8}, annot_kws={'size': 10, 'weight': 'bold'})
-    ax.set_title('Корреляционная матрица числовых признаков', fontsize=18, fontweight='bold', pad=20)
-    st.pyplot(fig)
+st.subheader('Топ-5 признаков, связанных с депрессией')
+correlation_matrix = data.select_dtypes(include='number').corr()
+top_features_corr = correlation_matrix['Depression'].abs().sort_values(ascending=False).head(6).index
+top_features_corr = top_features_corr.drop('Depression')
 
-    st.subheader('Топ-5 признаков, связанных с депрессией')
-    correlation_matrix = data.select_dtypes(include='number').corr()
-    top_features_corr = correlation_matrix['Depression'].abs().sort_values(ascending=False).head(6).index
-    top_features_corr = top_features_corr.drop('Depression')
+fig, ax = plt.subplots(figsize=(12, 8))
+sns.boxplot(data=data[top_features_corr], palette='Set2', ax=ax)
+ax.set_title('Топ признаки, связанные с депрессией', fontsize=16, fontweight='bold')
+st.pyplot(fig)
 
-    fig, ax = plt.subplots(figsize=(12, 8))
-    sns.boxplot(data=data[top_features_corr], palette='Set2', ax=ax)
-    ax.set_title('Топ признаки, связанные с депрессией', fontsize=16, fontweight='bold')
-    st.pyplot(fig)
+ordinal_mapping = {
+    'Sleep Duration': {'Less than 5 hours': 1, '5-6 hours': 2, '7-8 hours': 3, 'More than 8 hours': 4, 'Others': 0},
+    'Dietary Habits': {'Unhealthy': 1, 'Moderate': 2, 'Healthy': 3, 'Others': 0}
+}
+for col, mapping in ordinal_mapping.items():
+    data[col] = data[col].map(mapping)
 
+binary_columns = ['Have you ever had suicidal thoughts ?', 'Family History of Mental Illness']
+for col in binary_columns:
+    data[col] = data[col].map({'Yes': 1, 'No': 0})
 
-    
-    st.write("Препроцессинг завершен!")
+data = data.drop(columns=['id', 'Age', 'Degree', 'Profession', 'Work Pressure', 'City', 'Gender'])
+X = data.drop(columns=['Depression'])
+y = data['Depression']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-elif option == 'Модели и результаты':
-    ordinal_mapping = {
-        'Sleep Duration': {'Less than 5 hours': 1, '5-6 hours': 2, '7-8 hours': 3, 'More than 8 hours': 4, 'Others': 0},
-        'Dietary Habits': {'Unhealthy': 1, 'Moderate': 2, 'Healthy': 3, 'Others': 0}
-    }
-    for col, mapping in ordinal_mapping.items():
-        data[col] = data[col].map(mapping)
+models = {
+    'Random Forest': RandomForestClassifier(n_estimators=80, random_state=42),
+    'Gradient Boosting': GradientBoostingClassifier(random_state=42),
+    'Logistic Regression': LogisticRegression(random_state=42),
+    'XGBoost': xgb.XGBClassifier(n_estimators=100, random_state=42)
+}
 
-    binary_columns = ['Have you ever had suicidal thoughts ?', 'Family History of Mental Illness']
-    for col in binary_columns:
-        data[col] = data[col].map({'Yes': 1, 'No': 0})
+results = pd.DataFrame(columns=['Модель', 'Train ROC AUC', 'Test ROC AUC'])
 
-    data = data.drop(columns=['id', 'Age', 'Degree', 'Profession', 'Work Pressure', 'City', 'Gender'])
-    X = data.drop(columns=['Depression'])
-    y = data['Depression']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
-
-    models = {
-        'Random Forest': RandomForestClassifier(n_estimators=80, random_state=42),
-        'Gradient Boosting': GradientBoostingClassifier(random_state=42),
-        'Logistic Regression': LogisticRegression(random_state=42),
-        'XGBoost': xgb.XGBClassifier(n_estimators=100, random_state=42)
-    }
-
-    results = pd.DataFrame(columns=['Модель', 'Train ROC AUC', 'Test ROC AUC'])
-
-    for name, model in models.items():
-        model.fit(X_train, y_train)
-        y_train_proba = model.predict_proba(X_train)[:, 1]
-        y_test_proba = model.predict_proba(X_test)[:, 1]
-        train_roc_auc = roc_auc_score(y_train, y_train_proba)
-        test_roc_auc = roc_auc_score(y_test, y_test_proba)
-
-        results = pd.concat([results, pd.DataFrame({
-            'Модель': [name],
-            'Train ROC AUC': [train_roc_auc],
-            'Test ROC AUC': [test_roc_auc]
-        })], ignore_index=True)
-
-    st.write("Результаты моделей:")
-    st.write(results)
-
-    st.subheader('ROC-Кривая')
-    fig, ax = plt.subplots(figsize=(10, 8))
-    for name, model in models.items():
-        model.fit(X_train, y_train)
-        y_proba = model.predict_proba(X_test)[:, 1]
-        fpr, tpr, _ = roc_curve(y_test, y_proba)
-        roc_auc = auc(fpr, tpr)
-        ax.plot(fpr, tpr, lw=2, label=f'{name} (AUC = {roc_auc:.2f})')
-
-    ax.plot([0, 1], [0, 1], color='black', linestyle='--')
-    ax.set_xlabel('Уровень ложных срабатываний')
-    ax.set_ylabel('Уровень истинных срабатываний')
-    ax.set_title('Кривая приема-отклонения (ROC)')
-    ax.legend(loc='lower right')
-    ax.grid(True)
-    st.pyplot(fig)
-
-    st.subheader('Важность признаков')
-    model = RandomForestClassifier(n_estimators=80, random_state=42)
+for name, model in models.items():
     model.fit(X_train, y_train)
-    importance = model.feature_importances_
-    features = X_train.columns
-    importance_df = pd.DataFrame({'Feature': features, 'Importance': importance}).sort_values(by='Importance', ascending=False)
+    y_train_proba = model.predict_proba(X_train)[:, 1]
+    y_test_proba = model.predict_proba(X_test)[:, 1]
+    train_roc_auc = roc_auc_score(y_train, y_train_proba)
+    test_roc_auc = roc_auc_score(y_test, y_test_proba)
+    
+    results = pd.concat([results, pd.DataFrame({
+        'Модель': [name],
+        'Train ROC AUC': [train_roc_auc],
+        'Test ROC AUC': [test_roc_auc]
+    })], ignore_index=True)
 
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.barh(importance_df['Feature'], importance_df['Importance'], color='purple')
-    ax.set_xlabel('Важность')
-    ax.set_ylabel('Признаки')
-    ax.set_title('Важность признаков')
-    ax.invert_yaxis()
-    st.pyplot(fig)
+st.write("Результаты моделей:")
+st.write(results)
 
-    st.subheader('Гистограмма предсказанных вероятностей')
-    y_prob = model.predict_proba(X_test)[:, 1]
+st.subheader('ROC-Кривая')
+fig, ax = plt.subplots(figsize=(10, 8))
+for name, model in models.items():
+    model.fit(X_train, y_train)
+    y_proba = model.predict_proba(X_test)[:, 1]
+    fpr, tpr, _ = roc_curve(y_test, y_proba)
+    roc_auc = auc(fpr, tpr)
+    ax.plot(fpr, tpr, lw=2, label=f'{name} (AUC = {roc_auc:.2f})')
 
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.hist(y_prob[y_test == 1], bins=20, alpha=0.6, color='blue', label='На самом деле Yes')
-    ax.hist(y_prob[y_test == 0], bins=20, alpha=0.6, color='red', label='На самом деле No')
-    ax.set_xlabel('Предсказанная вероятность')
-    ax.set_ylabel('Частота')
-    ax.set_title('Гистограмма предсказанных вероятностей')
-    ax.legend(loc='upper center')
-    st.pyplot(fig)
+ax.plot([0, 1], [0, 1], color='black', linestyle='--')
+ax.set_xlabel('Уровень ложных срабатываний')
+ax.set_ylabel('Уровень истинных срабатываний')
+ax.set_title('Кривая приема-отклонения (ROC)')
+ax.legend(loc='lower right')
+ax.grid(True)
+st.pyplot(fig)
+
+st.subheader('Важность признаков')
+model = RandomForestClassifier(n_estimators=80, random_state=42)
+model.fit(X_train, y_train)
+importance = model.feature_importances_
+features = X_train.columns
+importance_df = pd.DataFrame({'Feature': features, 'Importance': importance}).sort_values(by='Importance', ascending=False)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.barh(importance_df['Feature'], importance_df['Importance'], color='purple')
+ax.set_xlabel('Важность')
+ax.set_ylabel('Признаки')
+ax.set_title('Важность признаков')
+ax.invert_yaxis()
+st.pyplot(fig)
+
+st.subheader('Гистограмма предсказанных вероятностей')
+y_prob = model.predict_proba(X_test)[:, 1]
+
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.hist(y_prob[y_test == 1], bins=20, alpha=0.6, color='blue', label='На самом деле Yes')
+ax.hist(y_prob[y_test == 0], bins=20, alpha=0.6, color='red', label='На самом деле No')
+ax.set_xlabel('Предсказанная вероятность')
+ax.set_ylabel('Частота')
+ax.set_title('Гистограмма предсказанных вероятностей')
+ax.legend(loc='upper center')
+st.pyplot(fig)
