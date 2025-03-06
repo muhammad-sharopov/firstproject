485668f965c20b7ee1ce2506bdddd41ebffa9946
muhammad-sharopov <82517976+muhammad-sharopov@users.noreply.github.com>
2025-03-06 22:59:55 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,11 +51,12 @@ elif option == 'Пропущенные значения':
        
 # Диаграмма распределения по полу
 st.write('### Распределение по полу')
-gender_counts = data['Gender'].value_counts().reset_index()
-gender_counts.columns = ['Gender', 'Count']
-fig_gender = px.pie(gender_counts, names='Gender', values='Count', title='Распределение по полу',
-                    color='Gender', color_discrete_map={'Male': '#66b3ff', 'Female': '#ff9999'})
-st.plotly_chart(fig_gender)
+gender_counts = data['Gender'].value_counts()
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.pie(gender_counts, labels=gender_counts.index, autopct='%1.1f%%', startangle=90,
+       colors=['#66b3ff', '#ff9999'], shadow=True, explode=(0.05, 0), textprops={'fontsize': 14})
+ax.set_title('Распределение по полу', fontsize=16, fontweight='bold', pad=20)
+st.pyplot(fig)
 
 # Гистограмма распределения по возрасту
 st.write('### Распределение по возрасту')
