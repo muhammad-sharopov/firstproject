def3e3efa37d3b7200235bc9681ec7726bbb094f
muhammad-sharopov <82517976+muhammad-sharopov@users.noreply.github.com>
2025-03-07 17:27:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 import xgboost as xgb
-from sklearn.metrics import roc_auc_score, accuracy_score, roc_curve, auc
+from sklearn.metrics import roc_auc_score, recall_score, precision_score, f1_score
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import GridSearchCV
